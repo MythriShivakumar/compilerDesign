43d8f48a71b3e25e9a0a999879b7afeb0c454582
MythriShivakumar <55782953+MythriShivakumar@users.noreply.github.com>
2021-07-30 19:32:05 +0530

    Lexical Analyser -1

---
+++ lexicalAnalyzer.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char string[20];
+    int i = 0;
+    int n = 1;
+    printf("Enter the string ");
+    fgets(string, 20, stdin);
+    for (i = 0; i < strlen(string); i++)
+    {
+        if (string[i] >= 'A' && string[i] <= 'Z' || string[i] >= 'a' && string[i] <= 'z')
+        {
+            if (string[i + 1] == '+' || string[i + 1] == '-' || string[i + 1] == '*' || string[i + 1] == '/' || string[i + 1] == '=' || string[i + 1] == '\0')
+            {
+                printf("id%d", n);
+                n++;
+            }
+        }
+
+        else if (string[i] >= 48 && string[i] <= 57)
+        {
+            printf("%c", string[i]);
+        }
+        else
+        {
+            switch (string[i])
+            {
+            case '+':
+                printf("+");
+                break;
+            case '-':
+                printf("-");
+                break;
+            case '*':
+                printf("*");
+                break;
+            case '/':
+                printf("/");
+                break;
+            case '\n':
+                printf(" ");
+                break;
+            case '=':
+                printf("=");
+                break;
+            default:
+                printf("Error");
+            }
+        }
+    }
+}
